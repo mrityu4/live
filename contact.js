61d0dcd0ab0b409cca92adbb550cb44b1ff3c61f
mrityu4 <48302684+mrityu4@users.noreply.github.com>
2020-10-22 23:21:36 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,53 +1,18 @@
 let grid = document.getElementById("contacts");
-grid.style.fontSize='x-large';
-let s="callicon";
-navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-
-function addcontact(id){
-    let diff='contact';
-    let curcontact = grid.insertRow(0);
-    curcontact.id = id+diff;
-    curcontact.innerHTML=userNameOf[id];
-    curcontact.style.color='white';
-    let callicon=curcontact.insertCell(0);
-    callicon.id=id+s;
-    callicon.innerHTML='<img src="./35459.svg" width="25px"/>';
-    callicon.value="call";
-    callicon.addEventListener("click", handlecall);
-    // 35459.svg-call
-    // 35364.svg-ring
-    //35225.svg-cut
-
-    
-    // console.log(userNameOf[id],userNameOf,id);
+grid.style.fontSize = 'x-large';
+let s = "callicon";
+let contactcounter = 0;
+let curcontact = grid.insertRow(contactcounter++);
+curcontact.innerHTML = "&emsp; &emsp;PEERS   ";
+curcontact.style.color = 'white';
+curcontact = grid.insertRow(contactcounter++);
+curcontact.innerHTML = myUserName + "(You)";
+curcontact.style.color = 'white';
+function addContact(id) {
+    let diff = 'contact';
+    console.log(id);
+    curcontact = grid.insertRow(contactcounter++);
+    curcontact.id = id + diff;
+    curcontact.innerHTML = userNameOf[id];
+    curcontact.style.color = 'white';
 }
-let myaudiostream;
-function handlecall(e){
-    let calliconid=e.target.id;
-    let callerid = e.target.id;
-    callerid=callerid-s;
-    if(e.target.value=="call"){
-        navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-        navigator.getUserMedia (
-            {video: false, audio: true},
-            function success(localAudioStream) {
-                myaudiostream= localAudioStream;
-            },
-            function error(err) {
-                    console.log("no audio permission",err);
-                }
-            );
-            let outgoingcall = peer.call(callerid, myaudiostream);
-            console.log("calling");
-
-
-    }    
-}
-peer.on('call', function(incoming) {
-    console.log("call received");
-    incoming.on('stream', function(stream) {
-        var audio = $('<audio autoplay />').appendTo('body');
-        audio[0].src = (URL || webkitURL || mozURL).createObjectURL(stream);      
-    });
-  });
-  
